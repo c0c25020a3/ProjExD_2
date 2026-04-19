dd617660ec7752064466d70931826a409faff24d
C0C25020 <c0c25020a3@edu.teu.ac.jp>
2026-04-19 19:09:06 +0900

    練習問題2

--- ex2/dodge_bomb.py
+++ ex2/dodge_bomb.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 import pygame as pg
 
 
@@ -24,6 +25,13 @@ def main():
     clock = pg.time.Clock()
     tmr = 0
 
+    bb_img = pg.Surface((20, 20))
+    bb_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx, vy = +5, +5
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -40,6 +48,10 @@ def main():
 
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
