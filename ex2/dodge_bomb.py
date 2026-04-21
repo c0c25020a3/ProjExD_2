5f7bf118a8021ca2114416f321d0629822de7347
C0C25020 <c0c25020a3@edu.teu.ac.jp>
2026-04-21 10:20:31 +0900

    追加機能2

--- ex2/dodge_bomb.py
+++ ex2/dodge_bomb.py
@@ -128,16 +128,12 @@ def main():
             kk_rct.move_ip(0, -sum_mv[1])
         screen.blit(kk_img, kk_rct)
 
-        idx = min(tmr // 500, 9)
-        bb_img = bb_imgs[idx]
-
-        old_center = bb_rct.center
-        bb_rct = bb_img.get_rect()
-        bb_rct.center = old_center
-
-        avx = vx * bb_accs[idx]
-        avy = vy * bb_accs[idx]
+        avx = vx * bb_accs[min(tmr//500, 9)]
+        avy = vy * bb_accs[min(tmr//500, 9)]
         bb_rct.move_ip(avx, avy)
+        bb_img = bb_imgs[min(tmr//500, 9)]
+        bb_rct.width = bb_img.get_rect().width
+        bb_rct.height = bb_img.get_rect().height
 
         yoko, tate = check_bound(bb_rct)
         if not yoko:
